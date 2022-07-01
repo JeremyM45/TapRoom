33e8885ca6a2c9700bb39cb5ca1506b83f7b9c21
JeremyM45 <jeremyvs1945@gmail.com>
2022-07-01 10:17:01 -0700

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return (
+    <React.Fragment>
+      <h3>Willkommen in Heinrichs Kneipe</h3>
+      <h5>Sehen Sie sich unsere Auswahl an Bieren an</h5>
+    </React.Fragment>
+  );
+}
+
+export default Header;
