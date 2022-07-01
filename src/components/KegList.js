0f22bd78e8a6f19530e7b5d481b7ffff10954cdb
JeremyM45 <jeremyvs1945@gmail.com>
2022-07-01 10:42:45 -0700

    add KegList component

---
+++ src/components/KegList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Keg from './Keg';
+import PropTypes from 'prop-types';
+
+function KegList(props) {
+  return (
+    <React.Fragment>
+      {props.kegList.map((keg) => 
+        <Keg
+        whenKegClicked = {props.onKegSelection}
+        name = {keg.name}
+        brand = {keg.brand}
+        price = {keg.price}
+        abv = {keg.abv}
+        id = {keg.id}
+        key = {keg.id} />
+      )}
+    </React.Fragment>
+  );
+}
+KegList.protoTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
+};
+export default KegList;
